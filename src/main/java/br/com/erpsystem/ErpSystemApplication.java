7ec317c6965f0acb617286ccf6298944402cf620
kawemorais <mcgamerz2013@gmail.com>
2023-07-30 02:51:54 -0300

    Configuracao inicial do projeto

---
+++ src/main/java/br/com/erpsystem/ErpSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.erpsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/erpsystem/ErpSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erpsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
