f9a674e94b0f058382194914e3560ff60abde877
kawemorais <mcgamerz2013@gmail.com>
2023-08-01 23:49:31 -0300

    fix: Corrigido os campos das tabelas - Foi adicionado AUTO_INCREMENT nos IDs das tabelas - Campo 'usuario' da tabela 'tb_usuarios_sistema' deve ser unico

--- src/main/resources/db/migration/V2__alterado_tabelas_uniades_e_categoriasproduto.sql
+++ src/main/resources/db/migration/V2__alterado_tabelas_uniades_e_categoriasproduto.sql
@@ -6,6 +6,9 @@ ALTER TABLE tb_unidades
 ALTER TABLE tb_categorias_produto
     MODIFY COLUMN codigo VARCHAR(10) UNIQUE NOT NULL;
 
+ALTER TABLE tb_usuarios_sistema
+    MODIFY COLUMN usuario VARCHAR(50) UNIQUE NOT NULL;
+
 ALTER TABLE tb_itens_fichatecnica
     MODIFY COLUMN quantidade DOUBLE NOT NULL,
     MODIFY COLUMN subTotal DECIMAL(15,4) NOT NULL;
